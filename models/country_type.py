2c97c65fd0c5bf54811b630176a4d5cfd6870ec2
Roksana Klodka <roksanaklodka101@gmail.com>
2025-06-05 22:21:58 +0200

    modelsy

---
+++ models/country_type.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class Country(str, Enum):
+    POLAND = "Poland"
+    GREECE = "Greece"
+    CROATIA = "Croatia"
+    ITALY = "Italy"
+    PORTUGAL = "Portugal"
+    FRANCE = "France"
+    SLOVAKIA = "Slovakia"
+    SWITZERLAND = "Switzerland"
+    SWEDEN = "Sweden"
+    NORWAY = "Norway"
+    FINLAND = "Finland"
